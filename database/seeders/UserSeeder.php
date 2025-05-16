04563cb6266c57b788c2b91e1450ca66443bd7db
Muhammad Murtadlo <muh.murtadlo23@gmail.com>
2025-05-16 22:56:50 +0700

    add UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'test',
+            'email' => 'test@gmail.com',
+            'phone' => '0822141454',
+            'password' => 'testtesttest',
+            'role' => 'ADMIN'
+        ]);
+    }
+}
