f97c5a24108b889fd573d28d01eee5ff99f4c4b0
Muhammad Murtadlo <muh.murtadlo23@gmail.com>
2025-05-27 00:52:52 +0700

    refactor route

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,13 @@ use App\Http\Controllers\UsersController;
 use App\Http\Middleware\CustomAuthenticate;
 use Illuminate\Support\Facades\Route;
 
+// auth route
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum', CustomAuthenticate::class])->group(function () {
+
+    // users route
     Route::prefix('users')->group(function () {
         Route::get('search', [UsersController::class, 'search'])->middleware('role:ADMIN');
 
@@ -28,12 +31,7 @@ Route::middleware(['auth:sanctum', CustomAuthenticate::class])->group(function (
         Route::get('profile', [UsersController::class, 'profile']);
     });
 
-    Route::prefix('users')->group(function () {
-        Route::post('{id}/docters', [DocterController::class, 'create'])
-            ->where(['id' => '[0-9]+'])
-        ;
-    })->middleware('role:ADMIN');
-
+    // docters route
     Route::prefix('docters')->group(function () {
         Route::post('', [DocterController::class, 'create'])
             ->middleware('role:ADMIN');
@@ -48,23 +46,30 @@ Route::middleware(['auth:sanctum', CustomAuthenticate::class])->group(function (
             ->middleware('role:ADMIN')
             ->where(['id' => '[0-9]+']);
     });
-    Route::post('patients', [PatientController::class, 'create']);
-    Route::get('/patients/{id}', [PatientController::class, 'findOne'])
-        ->where(['id' => '[0-9]+']);
-    Route::put('/patients/{id}', [PatientController::class, 'update'])
-        ->where(['id' => '[0-9]+']);
-    Route::delete('/patients/{id}', [PatientController::class, 'delete'])
-        ->where(['id' => '[0-9]+']);
-    Route::get('patients/search', [PatientController::class, 'search']);
 
+    // patients route
+    Route::prefix('patients')->group(function () {
+        Route::post('', [PatientController::class, 'create']);
+        Route::get('{id}', [PatientController::class, 'findOne'])
+            ->where(['id' => '[0-9]+']);
+        Route::put('{id}', [PatientController::class, 'update'])
+            ->where(['id' => '[0-9]+']);
+        Route::delete('{id}', [PatientController::class, 'delete'])
+            ->where(['id' => '[0-9]+']);
+        Route::get('search', [PatientController::class, 'search']);
+    });
 
-    Route::post('appointments', [AppointmentController::class, 'create']);
-    Route::put('appointments/{id}', [AppointmentController::class, 'update'])
-        ->where(['id' => '[0-9]+']);
-    Route::delete('appointments/{id}', [AppointmentController::class, 'delete'])
-        ->where(['id' => '[0-9]+']);
-    Route::get('appointments/search', [AppointmentController::class, 'search']);
+    // appointment route
+    Route::prefix('appointments')->group(function () {
+        Route::post('', [AppointmentController::class, 'create']);
+        Route::put('{id}', [AppointmentController::class, 'update'])
+            ->where(['id' => '[0-9]+']);
+        Route::delete('{id}', [AppointmentController::class, 'delete'])
+            ->where(['id' => '[0-9]+']);
+        Route::get('search', [AppointmentController::class, 'search']);
+    });
 
+    // medical_records route
     Route::prefix('medical_records')->group(function () {
         Route::post('', [MedicalRecordController::class, 'create']);
         Route::get('{id}', [MedicalRecordController::class, 'findOne'])
